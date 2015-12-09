47dffc5fb0144e5412135b15c58330f78151d521
Julius Garcia <garciajuliusb@yahoo.com>
2015-12-09 16:23:35 +0800

    bbbbbbbbbb

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -4,12 +4,27 @@ module.exports = [
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/com.catchoom.craftar/www/CraftARError.js",
+        "id": "com.catchoom.craftar.CraftARError",
+        "clobbers": [
+            "window.CraftARError"
+        ]
+    },
+    {
+        "file": "plugins/com.catchoom.craftar/www/CraftAR.js",
+        "id": "com.catchoom.craftar.CraftAR",
+        "clobbers": [
+            "window.CraftAR"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.0"
+    "cordova-plugin-whitelist": "1.2.0",
+    "com.catchoom.craftar": "0.5.2"
 }
 // BOTTOM OF METADATA
 });
